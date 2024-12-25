9dbfd27e650c00e3cb42baca30529fc14b455258
Javier Calartrava <javiercalatrava@capitole-consulting.com>
2024-12-25 12:43:34 +0100

    Initial Commit

---
+++ NFCApp/NFCAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NFCAppApp.swift
+//  NFCApp
+//
+//  Created by Javier Calatrava on 25/12/24.
+//
+
+import SwiftUI
+
+@main
+struct NFCAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
